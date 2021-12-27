1875486aa23a224ad1e90a97c064db668b6549c9
Leon Rosenshein <leonr@aurora.tech>
2021-12-26 19:47:07 -0700

    Working

---
+++ cmd/likes/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	LikesURL = "https://confluence.int.aurora.tech/rest/likes/1.0/content/%s/likes"
+	JiraHost = "https://confluence.int.aurora.tech/"
+	CqlURL   = "rest/api/content/?spaceKey=~leonr@aurora.tech&type=blogpost"
+)
+
+type blogEntry struct {
+	Id        string
+	Title     string
+	Likes     int
+	Published time.Time
+	Links     map[string]string `json:"_links"`
+}
+
+type cqlResponse struct {
+	Results []blogEntry
+	Start   int
+	Limit   int
+	Size    int
+	Links   map[string]string `json:"_links"`
+}
+
+type user struct {
+	Name                string
+	FullName            string
+	URL                 string
+	AvatarURL           string
+	FollowdByRemoteUser string
+}
+
+type like struct {
+	User user
+}
+
+type likesResponse struct {
+	Likes        []like
+	Content_Type string
+	Content_id   int
+}
+
+func main() {
+	allResults := []blogEntry{}
+	limit := 0
+	size := 0
+	base := JiraHost
+	next := CqlURL
+
+	jiraToken := os.Getenv("JIRA_TOKEN")
+	client := &http.Client{}
+
+	for limit == size {
+		url := base + next
+		req, _ := http.NewRequest("GET", url, nil)
+		req.Header.Set("Authorization", "Bearer "+jiraToken)
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyBytes, err := io.ReadAll(res.Body)
+		defer res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var blogs = cqlResponse{}
+		json.Unmarshal(bodyBytes, &blogs)
+		base = blogs.Links["base"]
+		next = blogs.Links["next"]
+		limit = blogs.Limit
+		size = blogs.Size
+		allResults = append(allResults, blogs.Results...)
+	}
+
+	for i, blog := range allResults {
+		url := fmt.Sprintf(LikesURL, blog.Id)
+		req, _ := http.NewRequest("GET", url, nil)
+		req.Header.Set("Authorization", "Bearer "+jiraToken)
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyBytes, err := io.ReadAll(res.Body)
+		defer res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var likes = likesResponse{}
+		json.Unmarshal(bodyBytes, &likes)
+		link := allResults[i].Links["webui"]
+		if strings.Contains(link, "display") {
+			parts := strings.Split(link, "/")
+			year, _ := strconv.ParseInt(parts[3], 10, 32)
+			month, _ := strconv.ParseInt(parts[4], 10, 32)
+			day, _ := strconv.ParseInt(parts[5], 10, 32)
+			allResults[i].Published = time.Date(int(year), time.Month(month), int(day), 0, 0, 0, 0, time.UTC)
+			//			fmt.Println(link)
+			//			fmt.Printf("Title: %s Year: %d Month: %d Day: %d Date: %s\n", blog.Title, year, month, day, allResults[i].Published)
+		}
+		allResults[i].Likes = len(likes.Likes)
+	}
+
+	sort.Slice(allResults, func(i, j int) bool {
+		return allResults[i].Likes < allResults[j].Likes
+	})
+
+	for _, blog := range allResults {
+		fmt.Printf("Title: %s Date:%s likes: %d\n", blog.Title, blog.Published.Format("2006-01-02"), blog.Likes)
+	}
+
+	fmt.Println(len(allResults))
+}
