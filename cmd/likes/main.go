e10fe5203d19ba71f901153b7821afd3998242a3
Leon Rosenshein <leonr@aurora.tech>
2021-12-28 10:02:07 -0700

    Refactor

--- cmd/likes/main.go
+++ cmd/likes/main.go
@@ -14,9 +14,11 @@ import (
 )
 
 const (
-	LikesURL = "https://confluence.int.aurora.tech/rest/likes/1.0/content/%s/likes"
-	JiraHost = "https://confluence.int.aurora.tech/"
-	CqlURL   = "rest/api/content/?spaceKey=~leonr@aurora.tech&type=blogpost"
+	LikesURL         = "https://confluence.int.aurora.tech/rest/likes/1.0/content/%s/likes"
+	JiraHost         = "https://confluence.int.aurora.tech/"
+	CqlURL           = "rest/api/content/?spaceKey=~leonr@aurora.tech&type=blogpost"
+	AuthHeaderFormat = "Bearer %s"
+	TokenPath        = "~/.jira/blogtoken"
 )
 
 type blogEntry struct {
@@ -53,66 +55,22 @@ type likesResponse struct {
 	Content_id   int
 }
 
+type JiraClient struct {
+	jiraToken string
+	client    *http.Client
+}
+
 func main() {
-	allResults := []blogEntry{}
-	limit := 0
-	size := 0
-	base := JiraHost
-	next := CqlURL
 
-	jiraToken := os.Getenv("JIRA_TOKEN")
-	client := &http.Client{}
-
-	for limit == size {
-		url := base + next
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Set("Authorization", "Bearer "+jiraToken)
-		res, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-		bodyBytes, err := io.ReadAll(res.Body)
-		defer res.Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var blogs = cqlResponse{}
-		json.Unmarshal(bodyBytes, &blogs)
-		base = blogs.Links["base"]
-		next = blogs.Links["next"]
-		limit = blogs.Limit
-		size = blogs.Size
-		allResults = append(allResults, blogs.Results...)
+	jiraClient, err := NewJiraClient()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	for i, blog := range allResults {
-		url := fmt.Sprintf(LikesURL, blog.Id)
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Set("Authorization", "Bearer "+jiraToken)
-		res, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-		bodyBytes, err := io.ReadAll(res.Body)
-		defer res.Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
+	allResults := LoadAllBlogs(jiraClient)
 
-		var likes = likesResponse{}
-		json.Unmarshal(bodyBytes, &likes)
-		link := allResults[i].Links["webui"]
-		if strings.Contains(link, "display") {
-			parts := strings.Split(link, "/")
-			year, _ := strconv.ParseInt(parts[3], 10, 32)
-			month, _ := strconv.ParseInt(parts[4], 10, 32)
-			day, _ := strconv.ParseInt(parts[5], 10, 32)
-			allResults[i].Published = time.Date(int(year), time.Month(month), int(day), 0, 0, 0, 0, time.UTC)
-			//			fmt.Println(link)
-			//			fmt.Printf("Title: %s Year: %d Month: %d Day: %d Date: %s\n", blog.Title, year, month, day, allResults[i].Published)
-		}
-		allResults[i].Likes = len(likes.Likes)
+	for i := range allResults {
+		GetBlogDetails(jiraClient, allResults, i)
 	}
 
 	sort.Slice(allResults, func(i, j int) bool {
@@ -122,6 +80,101 @@ func main() {
 	for _, blog := range allResults {
 		fmt.Printf("Title: %s Date:%s likes: %d\n", blog.Title, blog.Published.Format("2006-01-02"), blog.Likes)
 	}
+}
 
-	fmt.Println(len(allResults))
+func GetBlogDetails(jiraClient JiraClient, allResults []blogEntry, i int) {
+	url := fmt.Sprintf(LikesURL, allResults[i].Id)
+	req, _ := jiraClient.NewRequest(http.MethodGet, url, nil)
+	res, err := jiraClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyBytes, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var likes = likesResponse{}
+	json.Unmarshal(bodyBytes, &likes)
+	link := allResults[i].Links["webui"]
+	if strings.Contains(link, "display") {
+		parts := strings.Split(link, "/")
+		year, _ := strconv.ParseInt(parts[3], 10, 32)
+		month, _ := strconv.ParseInt(parts[4], 10, 32)
+		day, _ := strconv.ParseInt(parts[5], 10, 32)
+		allResults[i].Published = time.Date(int(year), time.Month(month), int(day), 0, 0, 0, 0, time.UTC)
+	}
+	allResults[i].Likes = len(likes.Likes)
+}
+
+func LoadAllBlogs(blogReader JiraClient) []blogEntry {
+	allResults := []blogEntry{}
+	limit := 0
+	size := 0
+	base := JiraHost
+	next := CqlURL
+	for limit == size {
+		url := base + next
+		blogs := LoadBlogsPage(blogReader, url)
+		allResults = append(allResults, blogs.Results...)
+		base = blogs.Links["base"]
+		next = blogs.Links["next"]
+		limit = blogs.Limit
+		size = blogs.Size
+	}
+	return allResults
+}
+
+func LoadBlogsPage(blogReader JiraClient, url string) cqlResponse {
+	req, _ := blogReader.NewRequest(http.MethodGet, url, nil)
+	res, err := blogReader.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyBytes, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var blogs = cqlResponse{}
+	json.Unmarshal(bodyBytes, &blogs)
+	return blogs
+}
+
+func NewJiraClient() (JiraClient, error) {
+	filePath := TokenPath
+	if strings.HasPrefix(filePath, "~/") {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return JiraClient{}, err
+		}
+		filePath = strings.Replace(filePath, "~", homeDir, 1)
+	}
+	jiraToken, err := os.ReadFile(filePath)
+	if err != nil {
+		return JiraClient{}, err
+	}
+
+	client := &http.Client{}
+	br := JiraClient{
+		jiraToken: string(jiraToken)[:len(jiraToken)-1],
+		client:    client,
+	}
+
+	return br, nil
+}
+
+func (b JiraClient) NewRequest(method string, url string, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf(AuthHeaderFormat, b.jiraToken))
+	return req, nil
+}
+
+func (b JiraClient) Do(request *http.Request) (*http.Response, error) {
+	return b.client.Do(request)
 }
